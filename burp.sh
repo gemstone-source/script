91ad92734e43c75f3a426e96a3fd4d33a8cc4ce9
gemstone-source <sophiajulius5@gmail.com>
2023-03-20 03:26:06 +0300

    Changing burp-pro script

--- burp.sh
+++ burp.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-cd /opt/burpsuite
+# Author Gemstone Hashghost
 
-java -javaagent:BurpSuiteLoader_v2022.8.jar --illegal-access=permit -Dfile.encoding=utf-8 -noverify -jar burpsuite_pro_v2022.8.jar
+# Navigating to the burpsuite folder
+# cd /opt/burpsuite
+
+# Load the burpsuite application
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED \
+--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED \
+--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED \
+-javaagent:/opt/burpsuite/Loader.jar -noverify -jar /opt/burpsuite/burpsuite_pro_v2023.2.2.jar
+
+# java -javaagent:BurpSuiteLoader_v2022.8.jar --illegal-access=permit -Dfile.encoding=utf-8 -noverify -jar burpsuite_pro_v2022.8.jar
