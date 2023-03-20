cd2a0a86d70baaecb51ca0dcf4ae71204a4db03e
gemstone-source <sophiajulius5@gmail.com>
2023-03-20 03:33:02 +0300

    Changing burp-pro script

--- burp.sh
+++ burp.sh
@@ -12,4 +12,6 @@ java --add-opens=java.desktop/javax.swing=ALL-UNNAMED \
 --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED \
 -javaagent:/opt/burpsuite/Loader.jar -noverify -jar /opt/burpsuite/burpsuite_pro_v2023.2.2.jar
 
+
+# Older script
 # java -javaagent:BurpSuiteLoader_v2022.8.jar --illegal-access=permit -Dfile.encoding=utf-8 -noverify -jar burpsuite_pro_v2022.8.jar
